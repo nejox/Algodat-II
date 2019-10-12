0057c24ec48b4c28c11cca77a18c6bf7fac19ffd
Jojo <Jojo@192.168.0.6>
2019-10-12 23:01:17 +0200

    Übung 1 erledigt

--- src/uebung1/HelloWorld.java
+++ src/uebung1/HelloWorld.java
@@ -1,7 +1,51 @@
 package uebung1;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 public class HelloWorld {
 	public static void main(String[] args) {
+
+		String output = "";
+
+		long start = System.nanoTime();
+		// 10x
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Hello World!");
+		}
+		long end = System.nanoTime();
+
+		output += "10x: " + (end-start) + "\n";
+
+		start = System.nanoTime();
+		// 100x
+		for (int i = 0; i < 100; i++) {
+			System.out.println("Hello World!");
+		}
+		end = System.nanoTime();
+
+		output += "100x: " + (end-start) + "\n";
+
+		start = System.nanoTime();
+		// 1000x
+		for (int i = 0; i < 1000; i++) {
+			System.out.println("Hello World!");
+		}
+		end = System.nanoTime();
+
+		output += "1.000x: " + (end-start) + "\n";
+
+		start = System.nanoTime();
+		// 10.000x
+		for (int i = 0; i < 10000; i++) {
+			System.out.println("Hello World!");
+		}
+		end = System.nanoTime();
+
+		output += "10.000x: " + (end-start) + "\n";
+		System.out.println(output);
+
 
 	}
 }
---
+++ src/uebung1/MatrixVektorMultiplikation.java
@@ -0,0 +1,63 @@
+package uebung1;
+
+public class MatrixVektorMultiplikation {
+	static double additionen = 0;
+	static double multiplikationen = 0;
+
+	public static void main(String[] args) {
+
+		double[][] matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+		double[] vektor = { 4, 4, 4 };
+
+		// Matrix-Vektor Multiplikation:
+//		double[] result = doMultiply(matrix, vektor);
+//		
+//		for (int i = 0; i < result.length; i++) {
+//			System.out.println(result[i]);
+//		}
+//		
+//		System.out.println("Additionen: "+additionen);
+//		System.out.println("Multiplikationen: "+multiplikationen);
+
+		// (A*x)/||A*x||
+		for (int j = 0; j < 10; j++) {
+
+			double[] result = doMultiply(matrix, vektor);
+			double betrag = calcBetragVektor(result);
+
+			for (int i = 0; i < result.length; i++) {
+				result[i] = result[i] / betrag;
+				System.out.println(result[i]);
+			}
+			
+			vektor = result;
+		}
+
+	}
+
+	public static double[] doMultiply(double[][] matrix, double[] vektor) {
+		double[] result = new double[matrix.length];
+
+		for (int i = 0; i < matrix.length; i++) {
+			double erg = 0;
+			for (int j = 0; j < matrix[i].length; j++) {
+				erg += matrix[i][j] * vektor[j];
+				multiplikationen++;
+				additionen++;
+			}
+			result[i] = erg;
+		}
+
+		return result;
+	}
+
+	public static double calcBetragVektor(double[] vektor) {
+		double erg = 0;
+
+		for (int i = 0; i < vektor.length; i++) {
+			erg += vektor[i] * vektor[i];
+		}
+
+		return Math.sqrt(erg);
+	}
+}
